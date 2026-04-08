408dc219db638bbe3ce698f435ad876c9b4cef75
root <root@srv644262.hstgr.cloud>
2026-04-08 16:41:44 +0000

    Resolve config overlap with master-M8V7o

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,10 @@ const withNextIntl = createNextIntlPlugin();
 
 const nextConfig: NextConfig = {
   output: "standalone",
+  serverExternalPackages: ["@prisma/client", "@prisma/adapter-pg", "pg", "bcryptjs"],
+  experimental: {
+    nodeMiddleware: true,
+  },
 };
 
 export default withNextIntl(nextConfig);
